f4198726d463a9b1b52650898249aae888904858
saintkliment <evgeniy122uryad@gmail.com>
2024-06-11 22:18:22 +0300

    add work_day for doc

--- project_doctors/app/business_logic.py
+++ project_doctors/app/business_logic.py
@@ -27,27 +27,27 @@ class employee_doctor():
         if self.rate == 1:
             doc_time_work = 8 # больше 8, но так, чтоб не больше 12 в неделю
             doc_time_chill = 60
-            doc_power_hour = 181 // doc_time_work
+            doc_power_hour = 20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.75:
             doc_time_work = 6
             doc_time_chill = 30
-            doc_power_hour =  181 // doc_time_work
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.5:
             doc_time_work = 4
             doc_time_chill = 30
-            doc_power_hour =  181 // doc_time_work
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.25:
             doc_time_work = 2
             doc_time_chill = 15
-            doc_power_hour =  181 // doc_time_work
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.1:
             doc_time_work = 2
             doc_time_chill = 15
-            doc_power_hour =  155 // 8
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
 
 
@@ -55,27 +55,27 @@ class employee_doctor():
         if self.rate == 1:
             doc_time_work = 8 # больше 8, но так, чтоб не больше 12 в неделю
             doc_time_chill = 1
-            doc_power_hour = 165 // 8
+            doc_power_hour = 20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.75:
             doc_time_work = 6
             doc_time_chill = 30
-            doc_power_hour =  165 // 8
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.5:
             doc_time_work = 4
             doc_time_chill = 30
-            doc_power_hour =  165 // 8
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.25:
             doc_time_work = 2
             doc_time_chill = 15
-            doc_power_hour =  165 // 8
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.1:
             doc_time_work = 2
             doc_time_chill = 15
-            doc_power_hour =  155 // 8
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
 
         
@@ -83,27 +83,27 @@ class employee_doctor():
         if self.rate == 1:
             doc_time_work = 8 # больше 8, но так, чтоб не больше 12 в неделю
             doc_time_chill = 1
-            doc_power_hour = 155 // 8
+            doc_power_hour = 20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.75:
             doc_time_work = 6
             doc_time_chill = 30
-            doc_power_hour =  155 // 8
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.5:
             doc_time_work = 4
             doc_time_chill = 30
-            doc_power_hour =  155 // 8
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.25:
             doc_time_work = 2
             doc_time_chill = 15
-            doc_power_hour =  155 // 8
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
         elif self.rate == 0.1:
             doc_time_work = 2
             doc_time_chill = 15
-            doc_power_hour =  155 // 8
+            doc_power_hour =  20 // doc_time_work
             return [doc_time_work, doc_time_chill, doc_power_hour]
 
 
@@ -124,142 +124,176 @@ class calculate_work_days():
         quantity_research_mrt_ky_2 = plan__[7]
         quantity_research_rg = plan__[8]
         quantity_research_flg = plan__[9]
-        duty_arr = []
+        # duty_arr = []
 
         while quantity_research_kt > 0:
             for i in self.doctors:
-                if i[1] == 'КТ':
-                    if quantity_research_kt > 0:
-                        rate = i[3]
-                        em = employee_doctor(rate)
-                        doc_capabilities = em.rate_calc_without_ky()
+                str_extra_modality = i[2]
+                if type(str_extra_modality) == str:
+                    doc_extra_modality = str_extra_modality.split(',')
+                for s in doc_extra_modality:
+                    if i[1] == 'КТ' or (s == ' КТ'):
+                        if quantity_research_kt > 0:
+                            rate = i[3]
+                            em = employee_doctor(rate)
+                            doc_capabilities = em.rate_calc_without_ky()
 
-                        doc_power = doc_capabilities[2] * doc_capabilities[0]
-                        quantity_research_kt -= doc_power
+                            doc_power = doc_capabilities[2] * doc_capabilities[0]
+                            quantity_research_kt -= doc_power
 
-                        doctor_name = i[0]
-                        db.add_work_day(doctor_name)
-                        
-                        print(quantity_research_kt, "quantity_research_kt")
+                            doctor_name = i[0]
+                            db.add_work_day(doctor_name, rate)
+
+                            print(quantity_research_kt, "quantity_research_kt")
         while quantity_research_kt_ky_1 > 0:
             for i in self.doctors:
-                if i[1] == 'КТ':
-                    if quantity_research_kt_ky_1 > 0:
-                        rate = i[3]
-                        em = employee_doctor(rate)
-                        doc_capabilities = em.rate_calc_with_ky()
+                str_extra_modality = i[2]
+                if type(str_extra_modality) == str:
+                    doc_extra_modality = str_extra_modality.split(',')
+                for s in doc_extra_modality:
+                    if i[1] == 'КТ' or (s == ' КТ'):
+                        if quantity_research_kt_ky_1 > 0:
+                            rate = i[3]
+                            em = employee_doctor(rate)
+                            doc_capabilities = em.rate_calc_with_ky()
 
-                        doc_power = doc_capabilities[2] * doc_capabilities[0]
-                        quantity_research_kt_ky_1 -= doc_power
+                            doc_power = doc_capabilities[2] * doc_capabilities[0]
+                            quantity_research_kt_ky_1 -= doc_power
 
-                        doctor_name = i[0]
-                        db.add_work_day(doctor_name)
-                        print(doctor_name, 'day + doc')
-                        
-                        print(quantity_research_kt_ky_1, "quantity_research_kt_ky_1")
+                            doctor_name = i[0]
+                            db.add_work_day(doctor_name, rate)
+                            print(doctor_name, 'day + doc')
+
+                            print(quantity_research_kt_ky_1, "quantity_research_kt_ky_1")
 
         while quantity_research_kt_ky_2 > 0:
             for i in self.doctors:
-                if i[1] == 'КТ':
-                    if quantity_research_kt_ky_2 > 0:
-                        rate = i[3]
-                        em = employee_doctor(rate)
-                        doc_capabilities = em.rate_calc_with_ky_2_plus()
+                str_extra_modality = i[2]
+                if type(str_extra_modality) == str:
+                    doc_extra_modality = str_extra_modality.split(',')
+                for s in doc_extra_modality:
+                    if i[1] == 'КТ' or (s == ' КТ'):
+                        if quantity_research_kt_ky_2 > 0:
+                            rate = i[3]
+                            em = employee_doctor(rate)
+                            doc_capabilities = em.rate_calc_with_ky_2_plus()
 
-                        doc_power = doc_capabilities[2] * doc_capabilities[0]
-                        quantity_research_kt_ky_2 -= doc_power
+                            doc_power = doc_capabilities[2] * doc_capabilities[0]
+                            quantity_research_kt_ky_2 -= doc_power
 
-                        doctor_name = i[0]
-                        db.add_work_day(doctor_name)
-                        print(doctor_name, 'day + doc')
-                        
-                        print(quantity_research_kt_ky_2, "quantity_research_kt_ky_2")
+                            doctor_name = i[0]
+                            db.add_work_day(doctor_name, rate)
+                            print(doctor_name, 'day + doc')
+
+                            print(quantity_research_kt_ky_2, "quantity_research_kt_ky_2")
 
         while quantity_research_mmg > 0:
             for i in self.doctors:
-                if i[1] == 'ММГ':
-                    if quantity_research_mmg > 0:
-                        rate = i[3]
-                        em = employee_doctor(rate)
-                        doc_capabilities = em.rate_calc_without_ky()
+                str_extra_modality = i[2]
+                if type(str_extra_modality) == str:
+                    doc_extra_modality = str_extra_modality.split(',')
+                for s in doc_extra_modality:
+                    if i[1] == 'ММГ' or (s == ' ММГ'):
+                        if quantity_research_mmg > 0:
+                            rate = i[3]
+                            em = employee_doctor(rate)
+                            doc_capabilities = em.rate_calc_without_ky()
 
-                        doc_power = doc_capabilities[2] * doc_capabilities[0]
-                        quantity_research_mmg -= doc_power
+                            doc_power = doc_capabilities[2] * doc_capabilities[0]
+                            quantity_research_mmg -= doc_power
 
-                        doctor_name = i[0]
-                        db.add_work_day(doctor_name)
-                        print(doctor_name, 'day + doc')
-                        
-                        print(quantity_research_mmg, "quantity_research_mmg")
+                            doctor_name = i[0]
+                            db.add_work_day(doctor_name, rate)
+                            print(doctor_name, 'day + doc')
+
+                            print(quantity_research_mmg, "quantity_research_mmg")
                         
         while quantity_research_mrt > 0:
             for i in self.doctors:
-                if i[1] == 'МРТ':
-                    if quantity_research_mrt > 0:
-                        rate = i[3]
-                        em = employee_doctor(rate)
-                        doc_capabilities = em.rate_calc_without_ky()
+                str_extra_modality = i[2]
+                if type(str_extra_modality) == str:
+                    doc_extra_modality = str_extra_modality.split(',')
+                for s in doc_extra_modality:
+                    if i[1] == 'МРТ' or (s == ' МРТ'):
+                        if quantity_research_mrt > 0:
+                            rate = i[3]
+                            em = employee_doctor(rate)
+                            doc_capabilities = em.rate_calc_without_ky()
 
-                        doc_power = doc_capabilities[2] * doc_capabilities[0]
-                        quantity_research_mrt -= doc_power
+                            doc_power = doc_capabilities[2] * doc_capabilities[0]
+                            quantity_research_mrt -= doc_power
 
-                        doctor_name = i[0]
-                        db.add_work_day(doctor_name)
-                        print(doctor_name, 'day + doc')
+                            doctor_name = i[0]
+                            db.add_work_day(doctor_name, rate)
+                            print(doctor_name, 'day + doc')
                         
-                        print(quantity_research_mrt, "quantity_research_mrt")
+                            print(quantity_research_mrt, "quantity_research_mrt")
 
         while quantity_research_mrt_ky_1 > 0:
             for i in self.doctors:
-                if i[1] == 'МРТ':
-                    if quantity_research_mrt_ky_1 > 0:
-                        rate = i[3]
-                        em = employee_doctor(rate)
-                        doc_capabilities = em.rate_calc_with_ky()
+                str_extra_modality = i[2]
+                if type(str_extra_modality) == str:
+                    doc_extra_modality = str_extra_modality.split(',')
+                
+                for s in doc_extra_modality:
+                    if i[1] == 'МРТ' or (s == ' МРТ'):
+                        if quantity_research_mrt_ky_1 > 0:
+                            rate = i[3]
+                            em = employee_doctor(rate)
+                            doc_capabilities = em.rate_calc_with_ky()
 
-                        doc_power = doc_capabilities[2] * doc_capabilities[0]
-                        quantity_research_mrt_ky_1 -= doc_power
+                            doc_power = doc_capabilities[2] * doc_capabilities[0]
+                            quantity_research_mrt_ky_1 -= doc_power
 
-                        doctor_name = i[0]
-                        db.add_work_day(doctor_name)
-                        print(doctor_name, 'day + doc')
+                            doctor_name = i[0]
+                            db.add_work_day(doctor_name, rate)
+                            print(doctor_name, 'day + doc')
                         
-                        print(quantity_research_mrt_ky_1, "quantity_research_mrt_ky_1")
+                            print(quantity_research_mrt_ky_1, "quantity_research_mrt_ky_1")
 
         while quantity_research_mrt_ky_2 > 0:
             for i in self.doctors:
-                if i[1] == 'МРТ':
-                    if quantity_research_mrt_ky_2 > 0:
-                        rate = i[3]
-                        em = employee_doctor(rate)
-                        doc_capabilities = em.rate_calc_with_ky_2_plus()
+                str_extra_modality = i[2]
+                if type(str_extra_modality) == str:
+                    doc_extra_modality = str_extra_modality.split(',')
+                for s in doc_extra_modality:
+                    if i[1] == 'МРТ' or (s == ' МРТ'):
+                        if quantity_research_mrt_ky_2 > 0:
+                            rate = i[3]
+                            em = employee_doctor(rate)
+                            doc_capabilities = em.rate_calc_with_ky_2_plus()
 
-                        doc_power = doc_capabilities[2] * doc_capabilities[0]
-                        quantity_research_mrt_ky_2 -= doc_power
+                            doc_power = doc_capabilities[2] * doc_capabilities[0]
+                            quantity_research_mrt_ky_2 -= doc_power
 
-                        doctor_name = i[0]
-                        db.add_work_day(doctor_name)
-                        print(doctor_name, 'day + doc')
+                            doctor_name = i[0]
+                            db.add_work_day(doctor_name, rate)
+                            print(doctor_name, 'day + doc')
                         
-                        print(quantity_research_mrt_ky_2, "quantity_research_mrt_ky_2")
+                            print(quantity_research_mrt_ky_2, "quantity_research_mrt_ky_2")
                         
         
         while quantity_research_rg > 0:
             for i in self.doctors:
-                if i[1] == 'РГ':
-                    if quantity_research_rg > 0:
-                        rate = i[3]
-                        em = employee_doctor(rate)
-                        doc_capabilities = em.rate_calc_without_ky()
+                str_extra_modality = i[2]
+                if type(str_extra_modality) == str:
+                    doc_extra_modality = str_extra_modality.split(',')
+                
+                for s in doc_extra_modality:
+                    if i[1] == 'РГ' or (s == ' РГ'):
+                        if quantity_research_rg > 0:
+                            rate = i[3]
+                            em = employee_doctor(rate)
+                            doc_capabilities = em.rate_calc_without_ky()
 
-                        doc_power = doc_capabilities[2] * doc_capabilities[0]
-                        quantity_research_rg -= doc_power
+                            doc_power = doc_capabilities[2] * doc_capabilities[0]
+                            quantity_research_rg -= doc_power
 
-                        doctor_name = i[0]
-                        db.add_work_day(doctor_name)
-                        print(doctor_name, 'day + doc')
+                            doctor_name = i[0]
+                            db.add_work_day(doctor_name, rate)
+                            print(doctor_name, 'day + doc')
                         
-                        print(quantity_research_rg, "quantity_research_rg")
+                            print(quantity_research_rg, "quantity_research_rg")
 
         while quantity_research_flg > 0:
             for i in self.doctors:
@@ -278,7 +312,7 @@ class calculate_work_days():
                             quantity_research_flg -= doc_power
 
                             doctor_name = i[0]
-                            db.add_work_day(doctor_name)
+                            db.add_work_day(doctor_name, rate)
                             print(doctor_name, 'day + doc')
                         
                         print(quantity_research_flg, "quantity_research_flg")
@@ -292,7 +326,6 @@ class calculate_work_days():
 
                 for s in doc_extra_modality:
                     if (i[1] == 'Денситометрия') or (s == ' Денситометрия'):
-                        print(1)
                         if quantity_research_dens > 0:
                             rate = i[3]
                             em = employee_doctor(rate)
@@ -302,13 +335,15 @@ class calculate_work_days():
                             quantity_research_dens -= doc_power
 
                             doctor_name = i[0]
-                            db.add_work_day(doctor_name)
+                            db.add_work_day(doctor_name, rate)
                             print(doctor_name, 'day + doc')
                         
-                            print(quantity_research_dens, "quantity_research_dens")
-        
+                            print(quantity_research_dens, "| quantity_research_dens")
+
 
         
 doctors = db.get_all_doctors()
 doc_calc = calculate_work_days(doctors)
-doc_calc.calc()
+# doc_calc.calc()
+
+# чекать дб и если + день, то минусуем, а иначе оставляем хп, как есть
--- project_doctors/app/models.py
+++ project_doctors/app/models.py
@@ -28,12 +28,37 @@ class Database:
     #         return False
     
 
-    def add_work_day(self, doctor_name):
+    def add_work_day(self, doctor_name, rate):
         try:
             self.__cur = self.conn.cursor()
-            self.__cur.execute(f"UPDATE work_days_doc SET days = days + 1 WHERE name = '{doctor_name}'")
-            self.conn.commit()
-            self.__cur.close()
+            self.__cur.execute(f"SELECT days FROM work_days_doc WHERE name = '{doctor_name}'")
+            work_days = self.__cur.fetchall()
+            work_days = work_days[0]
+            work_days = work_days[0]
+            print(work_days)
+            self.__cur.execute(f"SELECT rate FROM work_days_doc WHERE name = '{doctor_name}'")
+            rate_doc = self.__cur.fetchall()
+            rate_doc = rate_doc[0]
+            rate_doc = rate_doc[0]
+            print(rate_doc)
+            if work_days < 5 and rate_doc == 1:
+                self.__cur.execute(f"UPDATE work_days_doc SET days = days + 1  WHERE name = '{doctor_name}' ")
+                self.__cur.execute(f"UPDATE work_days_doc SET rate =  {rate}  WHERE name = '{doctor_name}' ")
+                self.conn.commit()
+                self.__cur.close()
+                return True
+            elif work_days < 5 and rate_doc == 0.75:
+                self.__cur.execute(f"UPDATE work_days_doc SET days = days + 1  WHERE name = '{doctor_name}' ")
+                self.__cur.execute(f"UPDATE work_days_doc SET rate =  {rate}  WHERE name = '{doctor_name}' ")
+                self.conn.commit()
+                self.__cur.close()
+                return True
+            elif work_days < 5:
+                self.__cur.execute(f"UPDATE work_days_doc SET days = days + 1  WHERE name = '{doctor_name}' ")
+                self.__cur.execute(f"UPDATE work_days_doc SET rate =  {rate}  WHERE name = '{doctor_name}' ")
+                self.conn.commit()
+                self.__cur.close()
+                return True
         except Exception as err:
             print(f"Ошибка: {err}")
             return False
