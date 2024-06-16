aa2e6bff02a2e8f8fdb64090eb82bff30dc707cd
saintkliment <evgeniy122uryad@gmail.com>
2024-06-16 17:08:58 +0300

    documentation adjustments

--- project_doctors/app/UserLogin.py
+++ project_doctors/app/UserLogin.py
@@ -1,4 +1,4 @@
-class UserLogin: # класс для реалищации логина в системе
+class UserLogin: # класс для реализации логина в системе
     def fromDB(self, user_id, db): # функция для получения данных из бд
         self.__user = db.getUser(user_id)  # само получение
         return self # возвращаем полученное
@@ -8,16 +8,16 @@ class UserLogin: # класс для реалищации логина в сис
         return self # возвращаем юзера
 
     def is_authenticated(self): # функция проверка авторизации
-        return True # подвержение правдой
+        return True # подтвержение правдой
     
-    def is_active(self): # функция автиный ли юзер сейчас
-        return True # подвержение правдой
+    def is_active(self): # функция активный ли юзер сейчас
+        return True # подтвержение правдой
     
-    def is_anonymous(self): # функция проверки юзер не авторизирован ли он
-        return False # возращение лжи
+    def is_anonymous(self): # функция проверки юзера на авторизацию
+        return False # возвращение лжи
      
     def get_id(self): # функция получения айди
-        return str(self.__user[0]) # возрат айди
+        return str(self.__user[0]) # возврат айди
 
     def get_role(self): # функция получения роли
         if len(self.__user) > 5: # проверка длины юзера
